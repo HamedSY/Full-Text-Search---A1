f3096228dbc935c07b5895356c4e9ce4239339f2
HamedSY <hamed007.saboor@gmail.com>
2023-07-08 15:50:02 +0330

    Create the set of all words

--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+namespace FullTextSearch
+{
+    
+    public class Program
+    {
+        private static readonly char[] delimeterChars = new char[] {' ', ',', '=', '-', '|', '>', '<', '(', ')', '?', '!', '.', '@', '/', '_', '\\', ':', '\"', '*'};
+        
+        public static HashSet<string> CreateSetOfWords()
+        {
+            
+            var documents = new List<string>();
+            foreach (string file in Directory.EnumerateFiles("EnglishData"))
+            {
+                string upperedfile = File.ReadAllText(file);
+                var splitedDocument = upperedfile.Split(delimeterChars);
+                documents.AddRange(splitedDocument);
+            }
+            return documents.ToHashSet();
+        }
+        public static void Main(string[] args)
+        {
+            var allWords = CreateSetOfWords();
+            foreach (var word in allWords)
+                Console.WriteLine(word);
+            
+
+        }
+    }
+}
