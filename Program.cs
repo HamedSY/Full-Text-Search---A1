30cadbbcf74250ab964f940f2910b6d38ef20a11
HamedSY <hamed007.saboor@gmail.com>
2023-07-08 16:39:56 +0330

    Create the inverted index dictionary

--- Program.cs
+++ Program.cs
@@ -3,29 +3,44 @@ using System.Collections;
 
 namespace FullTextSearch
 {
+    public class Document
+    {
+        public int Number { get; set; }
+        public string Content { get; set; }
+
+        public Document(int number, string content) {
+            Number = number;
+            Content = content;
+        }
+    }
     
     public class Program
     {
         private static readonly char[] delimeterChars = new char[] {' ', ',', '=', '-', '|', '>', '<', '(', ')', '?', '!', '.', '@', '/', '_', '\\', ':', '\"', '*'};
         
-        public static HashSet<string> CreateSetOfWords()
-        {
-            
-            var documents = new List<string>();
-            foreach (string file in Directory.EnumerateFiles("EnglishData"))
-            {
-                string upperedfile = File.ReadAllText(file);
-                var splitedDocument = upperedfile.Split(delimeterChars);
-                documents.AddRange(splitedDocument);
-            }
-            return documents.ToHashSet();
-        }
         public static void Main(string[] args)
         {
-            var allWords = CreateSetOfWords();
-            foreach (var word in allWords)
-                Console.WriteLine(word);
-            
+            var documents = new List<Document>();
+            var allWords = new List<string>();
+            foreach (string file in Directory.EnumerateFiles("EnglishData"))
+            {
+                var upperedfile = File.ReadAllText(file).ToUpper();
+                documents.Add(new Document(int.Parse(Path.GetFileName(file)), upperedfile));
+                var splitedDocument = upperedfile.Split(delimeterChars);
+                allWords.AddRange(splitedDocument);
+            }
+            var terms = allWords.ToHashSet().ToArray();
+            var invertedIndex = new Dictionary<string, List<int>>();
+
+            foreach (var term in terms)
+            {
+                var appropriateDocs = new List<int>();
+                foreach (var doc in documents)
+                    if (doc.Content.Split(delimeterChars).Contains(term))
+                        appropriateDocs.Add(doc.Number);
+
+                invertedIndex[term] = appropriateDocs;
+            }
 
         }
     }
